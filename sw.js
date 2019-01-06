4db3627b17724c5c0ef099fd1da0d14576b69dc3
perehelion <31827598+perehelion@users.noreply.github.com>
2019-01-06 13:18:54 +0200

    using workbox instead of toolbox

--- sw.js
+++ sw.js
@@ -1,7 +1,23 @@
 'use strict';
-importScripts('sw-toolbox.js');
-toolbox.precache(['index.html', 'style.css', 'script.js']);
-toolbox.router.get('./icons/*', toolbox.cacheFirst);
-toolbox.router.get('./*', toolbox.networkFirst, {
-  networkTimeoutSeconds: 5
-});
+// importScripts('sw-toolbox.js');
+// toolbox.precache(['index.html', 'style.css', 'script.js']);
+// toolbox.router.get('./icons/*', toolbox.cacheFirst);
+// toolbox.router.get('./*', toolbox.networkFirst, {
+//   networkTimeoutSeconds: 5
+// });
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+if (workbox) {console.log(`Yay! Workbox is loaded 🎉`)}
+workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+  workbox.strategies.networkFirst()
+);
+workbox.routing.registerRoute(
+  'index.html',
+  workbox.strategies.networkFirst()
+);
+workbox.routing.registerRoute(
+  'style.css',
+  workbox.strategies.networkFirst()
+);
